32f38fa510edae74677858b6120eea2822d74b2b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-13 01:03:47 +0700

    cleaning code dan upgrade ke viewBinding

--- app/src/main/java/com/gunder/kalkulator/MainActivity.kt
+++ app/src/main/java/com/gunder/kalkulator/MainActivity.kt
@@ -1,74 +1,77 @@
 package com.gunder.kalkulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.gunder.kalkulator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btn00.setOnClickListener { appendOnClick(true, "00") }
+            btn0.setOnClickListener { appendOnClick(true, "0") }
+            btn1.setOnClickListener { appendOnClick(true, "1") }
+            btn2.setOnClickListener { appendOnClick(true, "2") }
+            btn3.setOnClickListener { appendOnClick(true, "3") }
+            btn4.setOnClickListener { appendOnClick(true, "4") }
+            btn5.setOnClickListener { appendOnClick(true, "5") }
+            btn6.setOnClickListener { appendOnClick(true, "6") }
+            btn7.setOnClickListener { appendOnClick(true, "7") }
+            btn8.setOnClickListener { appendOnClick(true, "8") }
+            btn9.setOnClickListener { appendOnClick(true, "9") }
+            btnDots.setOnClickListener { appendOnClick(true, ".") }
+            //        membuat listener dari operator dalam kalkulator
+            btnPlus.setOnClickListener { appendOnClick(false, "+") }
+            btnMinus.setOnClickListener { appendOnClick(false, "-") }
+            btnMultiply.setOnClickListener { appendOnClick(false, "*") }
+            btnDivide.setOnClickListener { appendOnClick(false, "/") }
+            btnleftB.setOnClickListener { appendOnClick(false, "(") }
+            btnRightB.setOnClickListener { appendOnClick(false, ")") }
 
-//        membuat listener dari tombol
-        btn00.setOnClickListener { appendOnClick(true, "00") }
-        btn0.setOnClickListener { appendOnClick(true, "0") }
-        btn1.setOnClickListener { appendOnClick(true, "1") }
-        btn2.setOnClickListener { appendOnClick(true, "2") }
-        btn3.setOnClickListener { appendOnClick(true, "3") }
-        btn4.setOnClickListener { appendOnClick(true, "4") }
-        btn5.setOnClickListener { appendOnClick(true, "5") }
-        btn6.setOnClickListener { appendOnClick(true, "6") }
-        btn7.setOnClickListener { appendOnClick(true, "7") }
-        btn8.setOnClickListener { appendOnClick(true, "8") }
-        btn9.setOnClickListener { appendOnClick(true, "9") }
-        btnDot.setOnClickListener { appendOnClick(true, ".") }
-
-//        membuat listener dari operator dalam kalkulator
-        btnPlus.setOnClickListener { appendOnClick(false, "+") }
-        btnMinus.setOnClickListener { appendOnClick(false, "-") }
-        btnMultuply.setOnClickListener { appendOnClick(false, "*") }
-        btnDivide.setOnClickListener { appendOnClick(false, "/") }
-        btnLeftB.setOnClickListener { appendOnClick(false, "(") }
-        btnRightB.setOnClickListener { appendOnClick(false, ")") }
-
-        btnClear.setOnClickListener {
-            clear()
+            btnClear.setOnClickListener {
+                clear()
+            }
+            btnEquals.setOnClickListener {
+                calculate()
+            }
         }
-        btnEqual.setOnClickListener {
-            calculate()
-        }
+
+
     }
 
     private fun appendOnClick(clear: Boolean, string: String) {
         if (clear) {
-            tvOutput.text = ""
-            tvInput.append(string)
-        }else {
-            tvInput.append(tvOutput.text)
-            tvInput.append(string)
-            tvOutput.text = ""
+            binding.tvOutput.text = ""
+            binding.tvInput.append(string)
+        } else {
+            binding.tvInput.append(binding.tvOutput.text)
+            binding.tvInput.append(string)
+            binding.tvOutput.text = ""
         }
     }
 
     private fun clear() {
-        tvInput.text = ""
-        tvOutput.text = ""
+        binding.tvInput.text = ""
+        binding.tvOutput.text = ""
     }
 
     private fun calculate() {
         try {
-            val input = ExpressionBuilder(tvInput.text.toString()).build()
-            val outPut =input.evaluate()
+            val input = ExpressionBuilder(binding.tvInput.text.toString()).build()
+            val outPut = input.evaluate()
             val longOutput = outPut.toLong()
-            if (outPut == longOutput.toDouble()){
-                tvOutput.text = longOutput.toString()
+            if (outPut == longOutput.toDouble()) {
+                binding.tvOutput.text = longOutput.toString()
             } else {
-                tvOutput.text = outPut.toString()
+                binding.tvOutput.text = outPut.toString()
             }
         } catch (e: Exception) {
-            Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
         }
     }
 }
